00aa9d35a79baebbb915b3f82d7bc3da06f83e6e
apiper <apiper@126.com>
2025-08-20 15:26:57 +0800

    dotfiles nvim

---
+++ neovim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.swapfile = false
+vim.opt.autowrite = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
