d001d96a58b2ee4ac97d7471776c3d124aac3086
apiper0626 <47816635+apiper0626@users.noreply.github.com>
2026-06-29 10:54:07 +0800

    fix: vim plug bug and add coc-vim for autocompelte

--- vim/install.sh
+++ vim/install.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-PLUG_FILE=$HOME/.vim/autoload/plug.vim
-
-if [ -f "$PLUG_FILE" ]; then
-	echo "vim-plug exists, skip install"
-else
-	echo "开始安装vim-plug..."
-	curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	echo "完成安装vim-plug..."
-fi
-
 echo "soft link to vimrc"
 ln -sf "$PWD/vimrc" "$HOME/.vimrc"
